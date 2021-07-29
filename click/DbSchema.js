2fe3abb48016414944723b61d8b37b030a25cf2c
Douglas Naphas <douglasnaphas@gmail.com>
2021-07-28 21:59:11 -0400

    Remove un-needed files and packages
    gh-1

--- click/DbSchema.js
+++
@@ -1,8 +0,0 @@
-const schema = require('./schema');
-const getHash = require('./lib/getHash');
-class DbSchema {
-  static sortKeyFromGameName(gameName) {
-    return schema.PARTICIPANT_PREFIX + schema.SEPARATOR + getHash(gameName);
-  }
-}
-module.exports = DbSchema;
--- click/logger.js
+++
@@ -1,8 +0,0 @@
-const logger = {
-  log: msg => {
-    if(process && process.env && process.env.NODE_ENV !== 'test') {
-      console.log(msg);
-    }
-  }
-};
-module.exports = logger;
